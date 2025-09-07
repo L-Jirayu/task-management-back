90e0945b526c683c58f3daae93271feeef517e77
L-Jirayu <jilangza@gmail.com>
2025-09-07 23:02:08 +0700

    update commit

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const corsOptions = {
-    origin: "http://localhost:5173",
-  };
-  await app.enableCors(corsOptions);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.use(cookieParser());
+
+  app.enableCors({
+    origin: 'http://localhost:5173',           // ต้องระบุ origin ตรง ๆ ห้ามใช้ '*'
+    credentials: true,                          // <<< สำคัญมาก
+    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
+    exposedHeaders: [],                         // ถ้าอยาก expose header เพิ่มเติมค่อยใส่
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
-
